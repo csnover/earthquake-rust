23c84ef8f5521f050dc690c1f1e7657c763d0a36
Colin Snover <github.com@zetafleet.com>
2019-11-03 21:51:03 -0600

    Automatically look up resource forks when inspecting files

--- src/bin/inspect.rs
+++ src/bin/inspect.rs
@@ -1,8 +1,29 @@
 use earthquake::detect::detect_type;
-use std::{env, error::Error, fs::File, process::exit};
+use std::{env, error::Error, fs::File, io::{Error as IoError, ErrorKind, Result as IoResult}, path::PathBuf, process::exit};
+
+fn open_named_fork(filename: &str) -> IoResult<File> {
+    let path = format!("{}/..namedfork/rsrc", filename);
+    let metadata = std::fs::metadata(&path)?;
+    if metadata.len() > 0 {
+        File::open(&path)
+    } else {
+        Err(IoError::from(ErrorKind::NotFound))
+    }
+}
+
+fn open_apple_double(filename: &str) -> IoResult<File> {
+    let mut path = PathBuf::from(filename);
+    let filename = format!("._{}", path.file_name().unwrap().to_str().unwrap());
+    path.set_file_name(filename);
+    File::open(path)
+}
 
 fn read_file(filename: &str) -> Result<(), Box<dyn Error>> {
-    let mut file = File::open(filename)?;
+    let mut file = open_named_fork(&filename)
+        .or_else(|_| File::open(format!("{}.rsrc", filename)))
+        .or_else(|_| open_apple_double(&filename))
+        .or_else(|_| File::open(filename))?;
+
     match detect_type(&mut file) {
         Some(file_type) => { println!("{:?}", file_type); },
         None => { println!("{}: Invalid or unknown Director projector or movie.", filename); }
