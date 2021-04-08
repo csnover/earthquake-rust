ca775a6c1533e46808845257373218f5ea879789
Colin Snover <github.com@zetafleet.com>
2021-04-07 22:52:17 -0500

    Improve Xtra assertion messages

--- libearthquake/src/resources/xtra.rs
+++ libearthquake/src/resources/xtra.rs
@@ -6,12 +6,12 @@ use crate::util::RawString;
 pub struct Properties {
     // TODO: Load function should receive the global symbol table and be
     // converted to a symbol number instead of storing the name
-    #[br(assert(size >= name_size + 4))]
+    #[br(assert(size >= name_size + 4, "Xtra properties symbol name too big ({} < {})", size - 4, name_size))]
     name_size: u32,
     #[br(count = name_size)]
     symbol_name: RawString,
     // TODO: The rest.
-    #[br(assert(size >= data_size + name_size + 8))]
+    #[br(assert(size >= data_size + name_size + 8, "Xtra properties data too big ({} < {})", size - name_size - 8, data_size))]
     data_size: u32,
     #[br(count = data_size)]
     data: Vec<u8>,
